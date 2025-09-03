0ca01417ff5c0d7f36868735f0f2624a5eb968d9
Abzamora <amilcarbrandonzamoraparedes@gmail.com>
2025-09-03 11:15:53 -0400

    mi archivo operación python

---
+++ operacion.py
@@ -0,0 +1,9 @@
+numero1=5
+numero2= 10
+
+suma = numero1+numero2
+print("La suma de", numero1, "y", numero2, "es:", suma)
+print("1: Zamora Paredes Amilcar Brandon")
+print("2: Duran Alipaz Deysly Beatriz")
+print("3: Huanca Ancalle Lenny Daline")
+print("4: Serrano Mamani Raquel Araceli")
