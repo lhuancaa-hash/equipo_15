4e8efd00099a433c00d544adddca45f77f1a5359
Deysly Beatriz <deyslybeatrizduranalipaz@gmail.com>
2025-09-03 13:45:50 -0400

    Archivo java

---
+++ Saludo.java
@@ -0,0 +1,11 @@
+public class Saludo{
+    public static void main(String[] args){
+        System.out.println("HOLA, EQUIPO 15!!");
+        System.out.println("1: Duran Alipaz Deysly Beatriz");
+        System.out.println("2: Huanca Ancalle Lenny Daline");
+        System.out.println("3: Serrano Mamani Raquel Araceli");
+        System.out.println("4: Zamora Paredes Amilcar Brandon");
+        
+
+    }
+}
